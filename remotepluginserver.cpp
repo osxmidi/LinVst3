18b1c1d43f43f61ea2a8d080659b0834aa88d8f9
osxmidi <chopinbig@tutanota.com>
2019-08-16 22:05:56 +1000

    Update remotepluginserver.cpp

--- remotepluginserver.cpp
+++ remotepluginserver.cpp
@@ -1467,23 +1467,7 @@ void RemotePluginServer::dispatchParEvents()
     }
 #endif	    
 
-#ifdef MIDIEFF
-     case RemoteInProp:
-    {   
-        int index = readIntring(&m_shmControl5->ringBuffer);
-        bool b = getInProp(index);
-        tryWrite(&m_shm2[FIXED_SHM_SIZE2], &b, sizeof(bool));
-        break;
-    }
-    
-     case RemoteOutProp:
-    {   
-        int index = readIntring(&m_shmControl5->ringBuffer);
-        bool b = getOutProp(index);
-        tryWrite(&m_shm2[FIXED_SHM_SIZE2], &b, sizeof(bool));
-        break;
-    }
-    
+#ifdef MIDIEFF    
      case RemoteMidiKey:
     {   
         int index = readIntring(&m_shmControl5->ringBuffer);
