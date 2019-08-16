80f29b60ef9c2b47f026eda8697effcb25323e34
osxmidi <chopinbig@tutanota.com>
2019-08-16 22:08:59 +1000

    Update linvst.cpp

--- linvst.cpp
+++ linvst.cpp
@@ -643,15 +643,7 @@ VstIntPtr dispatcher(AEffect* effect, VstInt32 opcode, VstInt32 index, VstIntPtr
         break; 
 #endif      		    
 
-#ifdef MIDIEFF   
-    case effGetInputProperties:    
-        v = plugin->getEffInProp(index, (char *)ptr); 
-        break;  
-        
-    case effGetOutputProperties:    
-        v = plugin->getEffOutProp(index, (char *)ptr);
-        break; 
-        
+#ifdef MIDIEFF           
     case effGetMidiKeyName:    
         v = plugin->getEffMidiKey(index, (char *) ptr);
         break;   
