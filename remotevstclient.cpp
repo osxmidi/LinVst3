1ef06c3a217d3e8eecbcb3544eda1c50a31501a2
osxmidi <chopinbig@tutanota.com>
2022-05-04 17:48:46 +1000

    Update remotevstclient.cpp

--- remotevstclient.cpp
+++ remotevstclient.cpp
@@ -395,5 +395,11 @@ RemoteVSTClient::RemoteVSTClient(audioMasterCallback theMaster)
 }
 
 RemoteVSTClient::~RemoteVSTClient() {
-  //     wait(NULL);
+int pidval, wstatus;
+      for (int i = 0; i < 50000; i++) {
+      pidval = waitpid(-1, &wstatus, WNOHANG|WUNTRACED);
+      if (pidval <= 0)
+      break;
+      usleep(100);
+      }
 }
